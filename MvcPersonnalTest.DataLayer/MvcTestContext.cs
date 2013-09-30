3484dc4a42e202de536f3ad132c46d38d7260341
bmccoy04 <dembwl3@gmail.com>
2013-09-29 19:52:52 -0500

    Added db and some additional layers.

---
+++ MvcPersonnalTest.DataLayer/MvcTestContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvcPersonnalTest.BusinessModel;
+
+namespace MvcPersonnalTest.DataLayer
+{
+    public class MvcTestContext : ObjectContext
+    {
+        public MvcTestContext()
+            : base("name=MvcTestEntities", "MvcTestEntities")
+        {
+            _users = CreateObjectSet<User>();
+        }
+
+        public ObjectSet<User> Users
+        {
+            get
+            {
+                return _users;
+            }
+        }
+
+        private ObjectSet<User> _users;
+    }
+
+
+  
+}
